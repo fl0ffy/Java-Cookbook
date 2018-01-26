9a703d3c3f18160aff08d649f8e45a5e435f40ad
fl0ffy <bernat.johnathan@gmail.com>
2018-01-25 22:16:10 -0500

    pulled out some code from toolbar to p2gui

--- project_2/src/P2GUI.java
+++ project_2/src/P2GUI.java
@@ -7,11 +7,19 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class P2GUI extends JFrame {
+public class P2GUI extends JFrame implements ActionListener {
 
     private TextPanel textPanel;
+    private JButton withdrawBtn;
+    private JButton depositBtn;
+    private JButton transferToBtn;
+    private JButton balanceBtn;
     private Toolbar toolbar;
+    private StringListener textListener;
+    private JTextArea textArea;
 
     //constructor method
     public P2GUI () {
@@ -27,20 +35,59 @@ public class P2GUI extends JFrame {
         //create components
         textPanel = new TextPanel();
         toolbar = new Toolbar();
+        textArea = new JTextArea();
 
-        //toolbar.setTextPanel(textPanel);
+        withdrawBtn = new JButton("Withdrawl");
+        depositBtn = new JButton("Deposit");
+        transferToBtn = new JButton("Transfer To");
+        balanceBtn = new JButton("Balance");
+
+        withdrawBtn.addActionListener(this);
+        depositBtn.addActionListener(this);
+        transferToBtn.addActionListener(this);
+        balanceBtn.addActionListener(this);
+
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+        /*//toolbar.setTextPanel(textPanel);
         toolbar.setStringListenter(new StringListener() {
             public void textEmitted(String text) {
                 //System.out.print(text);
                 textPanel.appendText(text);
             }
-        });
+        });*/
 
         //add components to JFrame
-        add(toolbar, BorderLayout.NORTH);
+        //add(toolbar, BorderLayout.NORTH);
         add(textPanel, BorderLayout.CENTER);
+
     }
 
+    public void setStringListenter(StringListener listener){
+        this.textListener = listener;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JButton clicked = (JButton)e.getSource();
+
+        if (clicked == withdrawBtn) {
+            if(textListener != null) {
+                textListener.textEmitted("Withdraw\n");
+            }
+        } else if (clicked == depositBtn){
+            if(textListener != null) {
+                textListener.textEmitted("Deposit\n");
+            }
+        } else if (clicked == transferToBtn){
+            if(textListener != null) {
+                textListener.textEmitted("Transfer To\n");
+            }
+        } else if (clicked == balanceBtn){
+            if(textListener != null) {
+                textListener.textEmitted("Balance\n");
+            }
+        }
+    }
     //two account abjects (checking, savings)
 
     //Main method
